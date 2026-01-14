cc983d37ea130c8f4245feaa2c0278c94370256f
Sarthak Katkar <sarthakkatkar14@gmail.com>
2026-01-14 13:02:06 +0530

    Add demo files

---
+++ demo_01.js
@@ -0,0 +1,32 @@
+// var a = 200;
+// let ab = 300;
+// console.log(a);
+// console.log(ab);
+
+var a = 10;
+a = 200;
+let ab = 20;
+ab = 200;
+console.log(a);
+console.log(ab);
+
+// var a = 10;
+// console.log(a);
+
+// const b = 20;
+// console.log(b);
+
+// let c = 30;
+// console.log(c);
+
+var a = 10;
+let b = 20;
+
+{
+    var a = 15;
+    let b = 30;
+    console.log(a); // 15
+    console.log(b); // 30
+}
+console.log(a); // 15
+console.log(b); // 20
